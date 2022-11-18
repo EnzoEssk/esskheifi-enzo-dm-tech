0b8384c33688f53b8e432d7dbc72912f631d15a0
EnzoEssk <enzoesre@gmail.com>
2022-11-18 15:29:48 +0100

    Mise à jour DM

--- ex1.js
+++ ex1.js
@@ -1,8 +1,14 @@
-let Name;
-Name = 'Enzo Esskheifi'
-function reverseString(Name) {
-    return Name;
+function chaineReverse(mot) {
+
+  var motretourne = "";
+
+  for 
+  	(var z = mot.length - 1; z >= 0; z--) 
+  {
+
+      motretourne += mot[z];
+  }
+
+  return motretourne
 }
-console.log(Name)
-
-
+console.log(motretourneReverse('DM Tech'));
